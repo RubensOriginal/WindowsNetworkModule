4033f44cc98f091728233918e16001ec114a1471
RubensOriginal <gameplaysoriginal@gmail.com>
2023-02-26 00:47:52 -0300

    fix: fix open terminal when check ssid name, password and securityType

--- WifiModule/NetworkDataFinder.cs
+++ WifiModule/NetworkDataFinder.cs
@@ -57,7 +57,7 @@ namespace WifiModule
 
             process.StartInfo.Arguments = args;
 
-            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.UseShellExecute = true;
             process.StartInfo.RedirectStandardError = true;
             process.StartInfo.RedirectStandardInput = true;
             process.StartInfo.RedirectStandardOutput = true;
